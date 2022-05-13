295209019344ec78d5d4f5480a7ba33b3f0b3905
Valentin <Vinetos@users.noreply.github.com>
2022-05-13 08:18:52 +0200

    Create rust.nix

---
+++ rust.nix
@@ -0,0 +1,14 @@
+# Rust overlay
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "moz_overlay_shell";
+    buildInputs = [
+      # to use the latest nightly:
+      nixpkgs.latest.rustChannels.nightly.rust
+    ];
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  }
