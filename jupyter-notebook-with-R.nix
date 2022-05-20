db0a736e11febdf829ea84a9c437432bb850ab71
Valentin <Vinetos@users.noreply.github.com>
2022-05-20 08:56:34 +0200

    Create  jupyter-notebook-with-R.nix

---
+++ jupyter-notebook-with-R.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  jupyter = import (pkgs.fetchFromGitHub {
+    owner = "tweag";
+    repo = "jupyterWith";
+    # Replace this with current revision.
+    rev = "2b10030df2a29beed10c02d5f64745b143206350";
+    sha256 = "Yv52KWTmgzCb59sYcLc4bEn7LwZWI9E+EPgXzJiej64=";
+    fetchSubmodules = true;
+  }) {};
+
+  kernels = jupyter.kernels;
+
+  irkernel = kernels.iRWith {
+      name = "nixpkgs";
+      # Libraries to be available to the kernel.
+      packages = p: with p; [
+        ggplot2
+        dplyr
+        cowplot
+        car
+        lubridate
+      ];
+    };
+
+  jupyterEnvironment = (jupyter.jupyterlabWith {
+      kernels = [ irkernel ];
+    });
+in
+  jupyterEnvironment.env
